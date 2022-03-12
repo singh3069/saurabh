8c0dea946b0205a23e72c98fdeb419ae6075223f
rai10 <saurabhrai265@gmail.com>
2022-03-13 01:22:59 +0530

    added framer motion for animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
 
 function Header() {
   return (
-    <div>
-        <p>hi...!</p>
+    <div className="bg-darkBlue text-white">
+      <div className="flex">
+        <p className="text-3xl font-semibold font-serif">hi..</p>
+        <span className="text-3xl font-semibold font-serif">!</span>
+      </div>
       <p>Saurabh Here </p>
       <p>
         I learn stuff from the Internet by the Night,and build Websites by the
