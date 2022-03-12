7254ae486744fb0ee1c700ba47b9854da70f2dd1
rai10 <saurabhrai265@gmail.com>
2022-03-13 00:21:12 +0530

    started the new portfolio site

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+function Header() {
+  return (
+    <div>
+        <h1>hi...!</h1>
+      <p>Saurabh Here </p>
+      <p>
+        I learn stuff from the Internet by the Night,and build Websites by the
+        day{" "}
+      </p>
+    </div>
+  )
+}
+
+export default Header
